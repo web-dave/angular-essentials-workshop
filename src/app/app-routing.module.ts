9a5b462038f363c8f97d884de58b674d080014bf
web-dave <dave@webdave.de>
2022-06-15 14:45:15 +0200

    start routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/books',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
