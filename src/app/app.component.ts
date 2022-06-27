ab1d063210238cca3dd3da6dac394d2224ee3e4e
web-dave <dave@webdave.de>
2022-06-27 17:54:23 +0200

    template binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'zs-root',
   templateUrl: './app.component.html',
+  // template: `
+  //   <input [value]="title" (input)="updateT($event)" />
+  //   <button (click)="baz($event)">{{ i }}</button>
+  // `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-essential-workshop 🦄!!!!';
+  edit = true;
+  i = 0;
+  baz(evt?: MouseEvent) {
+    this.i++;
+    this.title = this.edit + '' + this.i;
+  }
+
+  updateT(evt: Event) {
+    this.title = (evt.target as HTMLInputElement).value;
+    console.log(evt);
+  }
+  constructor() {
+    // setInterval(() => {
+    //   this.baz();
+    // }, 1500);
+  }
 }
+
+const foo: any = {
+  bar: 1,
+  event: (evt: any) => {},
+};
+foo['bar'] = 4711;
+foo.event('click');
