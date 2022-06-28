5e3255065aa767a1d15b9995a758c2be0ca83a44
web-dave <dave@webdave.de>
2022-06-28 10:26:00 +0200

    use book component

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -4,14 +4,9 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './books.component';
 
-
 @NgModule({
-  declarations: [
-    BooksComponent
-  ],
-  imports: [
-    CommonModule,
-    BooksRoutingModule
-  ]
+  declarations: [BooksComponent],
+  exports: [BooksComponent],
+  imports: [CommonModule, BooksRoutingModule],
 })
-export class BooksModule { }
+export class BooksModule {}
