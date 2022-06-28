c158f15cf3a0e5aed04428d4c04bd58a28cbc479
web-dave <dave@webdave.de>
2022-06-28 15:15:32 +0200

    httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { AboutModule } from './about/about.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, TopNavComponent],
-  imports: [BrowserModule, AppRoutingModule, AboutModule],
+  imports: [BrowserModule, AppRoutingModule, AboutModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/books/book.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  root = 'http://localhost:4730/books';
+  constructor(private http: HttpClient) {}
+  getBooks(): Observable<any> {
+    return this.http.get(this.root);
+  }
+}
