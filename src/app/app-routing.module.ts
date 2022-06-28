417bec3e6cc3600f71928e29c5480e1728cb9cc2
web-dave <dave@webdave.de>
2022-06-28 14:00:33 +0200

    lazy loading und so

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { BooksComponent } from './books/books.component';
 
 const routes: Routes = [
   {
     path: 'books',
-    component: BooksComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
+    loadChildren: () =>
+      import('./books/books.module').then((m) => m.BooksModule),
   },
 ];
 
--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { BooksComponent } from './books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BooksComponent,
+    children: [
+      {
+        path: '',
+        component: BookListComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class BooksRoutingModule { }
+export class BooksRoutingModule {}
