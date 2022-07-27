111fd12457f064cb8486f486c70ddb37b9013d1e
web-dave <dave@webdave.de>
2022-07-27 11:45:59 +0200

    ngFor

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
-
-  constructor() { }
+  books: any[] = [];
+  constructor(private service: BookService) {}
 
   ngOnInit(): void {
+    this.service.getBooks().subscribe({
+      next: (data) => (this.books = data),
+      complete: () => console.log('DONE'),
+    });
   }
-
 }
