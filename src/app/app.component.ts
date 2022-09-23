779e721ae525fa35f39f77c7648f4ee5ccda55f2
web-dave <dave@webdave.de>
2022-09-23 08:58:14 +0200

    pipe showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-essential-workshop';
+  foo = 1;
   constructor() {
     setTimeout(() => (this.title = 'södjfkhsaöfh'), 1500);
+    setInterval(() => {
+      this.foo = 2;
+    }, 1500);
+  }
+
+  pipeIt(value: number): string {
+    console.log('pipeIt');
+    return value + '!';
   }
 }
