70793e7e23fa67097094fa5eed9d052b47161244
web-dave <dave@webdave.de>
2022-09-26 11:52:22 +0200

    NgFor

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry, catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +9,9 @@ export class BookService {
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<any> {
-    return this.http.get<any>('http://localhost:4730/books');
+    return this.http.get<any>('http://localhost:4730/books').pipe(
+      retry({ count: 2, delay: 1500 }),
+      catchError((err) => of([]))
+    );
   }
 }
